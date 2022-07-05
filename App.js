96ca0e2f78f6eac381f038e2f0bb4aae7eaffae2
BleBarak <blebarak1@gmail.com>
2022-07-05 12:26:52 +0200

    init

---
+++ App.js
@@ -0,0 +1,29 @@
+import { StatusBar } from 'expo-status-bar';
+import { Button, StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+export default function App() {
+  const [number, setNumber] = useState(0);
+  function counting() {
+    setNumber(number + 1);
+  }
+  function countingdown(){
+setNumber(number - 1)
+  }
+  return (
+    <View style={styles.container}>
+      <Text> <h1> IMUKS INTERNSHIP APP1</h1></Text>
+      <Text><h1>COUNT ={number}</h1></Text>
+      <Button title="COUNT" onPress={()=> counting()}></Button>
+      <Button title ="COUNTDOWN" onPress={()=> countingdown()}></Button>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'gray',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
