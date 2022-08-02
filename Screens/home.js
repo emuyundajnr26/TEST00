7454b1b2a43554b49afb170e772be750d99ba20d
BleBarak <blebarak1@gmail.com>
2022-08-02 16:01:25 +0200

    init

--- Screens/home.js
+++ Screens/home.js
@@ -1,22 +1,22 @@
-import { StyleSheet,Button, Text, View } from "react-native";
+import { StyleSheet, Button, Text, View } from "react-native";
 import React from "react";
 
-
-export default function Home ({navigation}){
-    return(
-        <View style={styles.main}> 
-        <Text>WELCOME HOME</Text>
-        <Button
-        title="CLICK TO START" onPress={ ()=> navigation.navigate("welcome")}></Button>
-        </View>
-    )
-
+export default function Home({ navigation }) {
+  return (
+    <View style={styles.main}>
+      <Text>WELCOME HOME</Text>
+      <Button
+        title="CLICK "
+        onPress={() => navigation.navigate("welcome")}
+      ></Button>
+    </View>
+  );
 }
 const styles = StyleSheet.create({
-    main: {
-        flex: 1, 
-        backgroundColor: "grey",
-        alignItems: "center",
-        justifyContent: "center",
-    }
-})
+  main: {
+    flex: 1,
+    backgroundColor: "grey",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
